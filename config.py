3bee4ded1efb645b444de1e527028e118dcb0ec9
Moulali1144 <MOULALI.MASTAN@GMAIL.COM>
2026-02-08 16:55:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,10 @@ import sys
 
 BOT_TOKEN = os.getenv("8233719276:AAFEUpZ7DsQ8jA18zuGLNZ6dIk5ld41NT7s")
 
+print("DEBUG BOT_TOKEN =", BOT_TOKEN)
+
 if not BOT_TOKEN:
-    print("❌ ERROR: BOT_TOKEN environment variable not set")
+    print("❌ BOT_TOKEN NOT FOUND")
     sys.exit(1)
 
 CHECK_INTERVAL = 300
