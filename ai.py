d6c7b95d54dbfcefd018e74a7ffc74055eb1f314
Moulali1144 <MOULALI.MASTAN@GMAIL.COM>
2026-02-08 13:31:33 +0530

    Add files via upload

--- ai.py
+++ ai.py
@@ -1,9 +1,11 @@
-from transformers import pipeline
-model = pipeline("sentiment-analysis", model="ProsusAI/finbert")
+from textblob import TextBlob
+
 def news_sentiment(news):
     score = 0
     for n in news:
-        r = model(n)[0]["label"]
-        if r == "positive": score += 1
-        elif r == "negative": score -= 1
+        polarity = TextBlob(n).sentiment.polarity
+        if polarity > 0.1:
+            score += 1
+        elif polarity < -0.1:
+            score -= 1
     return score
