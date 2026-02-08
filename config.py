b3e64e43a324f69a3c9c5e55dd70f024644804a5
Moulali1144 <MOULALI.MASTAN@GMAIL.COM>
2026-02-08 23:01:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,5 @@
-import os
-import sys
-from dotenv import load_dotenv
+# TEMPORARY SIMPLE CONFIG (NO ENV VARIABLES)
 
-load_dotenv()  # load .env file
-
-BOT_TOKEN = os.getenv("8233719276:AAFEUpZ7DsQ8jA18zuGLNZ6dIk5ld41NT7s")
-
-if not BOT_TOKEN:
-    print("❌ BOT_TOKEN NOT FOUND")
-    sys.exit(1)
+BOT_TOKEN = "8233719276:AAFEUpZ7DsQ8jA18zuGLNZ6dIk5ld41NT7s"
 
 CHECK_INTERVAL = 300
